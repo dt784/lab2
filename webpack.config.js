fc12e8df8d7e7ee7e3395260c1ae6ed1748b0f7e
jolyh1pp0 <104237627+dt784@users.noreply.github.com>
2022-04-23 03:26:59 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
+    },
+
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'index',
+            template: path.resolve(__dirname, './src/pages/index.html'),
+            filename: 'index.html',
+        }),
+
+        new HtmlWebpackPlugin({
+            title: 'about',
+            template: path.resolve(__dirname, './src/pages/about.html'),
+            filename: 'about.html',
+        }),
+
+        new CleanWebpackPlugin(),
+    ]
+}
